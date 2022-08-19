0f3c8a7e994344302fb512ba34f9f225db0b0d5a
tristan99 <ariescacy9@gmail.com>
2022-08-18 19:24:51 -0500

    mejorada

--- main.js
+++ main.js
@@ -1,51 +1,41 @@
-let c = console.log;
-let buscar = "Lima";
-const $cuidad = document.getElementById("cuidad")
-function upperA (str){
-  return str.split(" ").map(e => e[0].toUpperCase() + e.slice(1)).join(" ")
-}
-//const transformada = upperA ($cuidad);
+let key = "215ac86156a45affffa7e8fcf2f0c69f"
+let $button = document.getElementById("btn")
+let $search = document.getElementById("search")
 
-const $box = document.getElementById("container__box"),
-      fragmento = document.createDocumentFragment(),
-      url = "https://api.openweathermap.org/data/2.5/weather?q=Ayacucho&appid=215ac86156a45affffa7e8fcf2f0c69f&lang=es&units=metric";
-    
-function axiosAsync(){
-  async function geTdata(){
-    try {
-      let response = await axios.get(url),
-      obj = await response.data;
-      let array = [obj];
-      c(array);
-      array.forEach((e) => {
-        const li = document.createElement("li");
-        li.innerHTML = `<div class="tarjeta">
-                          <div class="descripcion">
-                            <h1>Buscador de Clima</h1>
-                            <h2>clima en ${e.name}</h2>
-                            <h3>CLIMA:${e.weather[0].main}</h3>
-                            <h4>Descripcion: ${e.weather[0].description}</h4>
-                            <img src="https://openweathermap.org/img/wn/${e.weather[0].icon}@4x.png" alt="img">
-                            <h3>temperatura ${e.main.temp} °C</h3>
-                            <p>Max: ${e.main.temp_max} °C </p>
-                            <p>Min: ${e.main.temp_min} °C </p>
-                            <input type="text" placeholder="Cuidad" id="cuidad">
-                            <button id="btn">Consulatr el Clima</button>
-                          </div>
-                        </div>`;
-        fragmento.appendChild(li);
-      });
-      $box.appendChild(fragmento);
-    } catch (error) {
-      let message =  "ocurrio un error";
-      $box.innerHTML = `Error ${message}`;
-    } finally {
-      c("siempre ejecutandose");
-    }
+$button.addEventListener("click", (e) => {
+  e.preventDefault()
+  let resultado = $search.value
+  let city = resultado
+  if (resultado === "") {
+    errorMensaje()
   }
-  geTdata()
-}
+  callCity(city)
+})
 
-document.getElementById("btn").addEventListener("click", ()=>{
-  axiosAsync();
-});
+
+function callCity(city) {
+
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=es&appid=${key}`
+
+  axios.get(url)
+    .then(response => {
+      this.users = response.data;
+      let clima = response.data;
+      let icon = response.data.weather[0].icon
+      let dato = response.data.weather[0].description
+      let result = `<h1>${response.data.name}</h1>
+                    <h3>${response.data.weather[0].description}</h3><br>
+                    <li>Temperatura:
+                      <h2>${Math.floor((response.data.main.temp))}°C<h2>
+                    </li>
+                    <li>Min: ${Math.floor(response.data.main.temp_min)}°</li>
+                    <li>Max: ${Math.floor(response.data.main.temp_max)}°</li>`
+      let img = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png">`
+      document.getElementById("container").innerHTML = result
+      document.getElementById("img-container").innerHTML = img
+    })
+    .catch(e => {
+      let message =  "ocurrio un error";
+      document.getElementById("container").innerHTML = message
+    })
+}
